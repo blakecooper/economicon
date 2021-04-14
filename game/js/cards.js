0dace66977e34282d292b2e574066b1f659ea9f9
Blake Cooper <blakecoo@buffalo.edu>
2021-04-14 12:49:36 -0400

    Wiped previous game

--- game/js/cards.js
+++
@@ -1,8 +0,0 @@
-function Card(name, text, satisfied, positive) {
-	this.name = name;
-	this.text = text;
-	this.satisfied = satisfied;
-	this.positive = positive;
-	this.negative = -1;
-	this.turnsLeft = -1;
-};
